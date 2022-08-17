cf6d7fe02e07b5b305c3347edffc00014e711d71
werewolf2150 <werewolf2150@hotmail.com>
2022-08-18 00:18:29 +0200

    Update Cs File

--- CryoFall.cs/CryoFall.cs
+++ CryoFall.cs/CryoFall.cs
@@ -18,11 +18,11 @@ namespace WindowsGSM.Plugins
         public Plugin Plugin = new Plugin
         {
             name = "WindowsGSM.CoreKeeper", // WindowsGSM.XXXX
-            author = "werewolf2150",
+            author = "Geekbee",
             description = "WindowsGSM plugin for supporting CoreKeeper Dedicated Server",
-            version = "1.1",
-            url = "https://github.com/werewolf2150/WindowsGSM.CoreKeeper", // Github repository link (Best practice)
-            color = "#34c9eb" // Color Hex
+            version = "1.0",
+            url = "https://github.com/GeekbeeGER/WindowsGSM.CoreKeeper", // Github repository link (Best practice)
+            color = "#34c9ec" // Color Hex
         };
 
         // - Settings properties for SteamCMD installer
@@ -36,18 +36,18 @@ namespace WindowsGSM.Plugins
 
 
         // - Game server Fixed variables
-        public override string StartPath => @"CoreKeeperServer.exe"; // Game server start path
+        public override string StartPath => @"Launch.bat"; // Game server start path
         public string FullName = "CoreKeeper Dedicated Server"; // Game server FullName
-        public bool AllowsEmbedConsole = false;  // Does this server support output redirect?
-        public int PortIncrements = 1; // This tells WindowsGSM how many ports should skip after installation
+        public bool AllowsEmbedConsole = true;  // Does this server support output redirect?
+        public int PortIncrements = 10; // This tells WindowsGSM how many ports should skip after installation
         public object QueryMethod = new A2S(); // Query method should be use on current server type. Accepted value: null or new A2S() or new FIVEM() or new UT3()
 
 
         // - Game server default values
-        public string Port = "9876"; // Default port
-        public string QueryPort = "9877"; // Default query port
-        public string Defaultmap = "world1"; // Default map name
-        public string Maxplayers = "10"; // Default maxplayers
+        public string Port = "9000"; // Default port
+        public string QueryPort = "9100"; // Default query port
+        public string Defaultmap = "map"; // Default map name
+        public string Maxplayers = "100"; // Default maxplayers
         public string Additional = ""; // Additional server start parameter
 
 
@@ -72,7 +72,7 @@ namespace WindowsGSM.Plugins
 
             // Prepare start parameter
 
-			string param = $"-batchmode -nographics {_serverData.ServerParam}" + (!AllowsEmbedConsole ? " -log" : string.Empty);	
+			string param = $"-batchmode {_serverData.ServerParam}" + (!AllowsEmbedConsole ? " -log" : string.Empty);	
 	
 
 
@@ -129,17 +129,8 @@ namespace WindowsGSM.Plugins
                 return null; // return null if fail to start
             }
         }
-
-
-// - Stop server function
-        public async Task Stop(Process p)
-        {
-            await Task.Run(() =>
-            {
-                 p.Kill();
-            });
-			 await Task.Delay(10000);
-        }
-
+	
+	   // - Stop server function
+	   public async Task Stop(Process p) => await Task.Run(() => { p.Kill(); }); // I believe Core Keeper don't have a proper way to stop the server so just kill it
     }
 }
